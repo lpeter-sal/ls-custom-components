5cfa8a96395bf1ac34da2291aa64aa10afa3dffa
Luis Pedro Salvador Hernandez <pedrosh09@gmail.com>
2025-02-25 14:11:14 -0700

    Ends package

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+
+
+export { MyLabel } from './components/MyLabel'
+export { Button } from './stories/Button'
+export { Header } from './stories/Header'
+export { Page } from './stories/Page'
+
+
+
+
+
+
+
+
+
